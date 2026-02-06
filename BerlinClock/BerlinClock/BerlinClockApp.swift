dbc38a767d6b1070bbd86c2aff5686415b3d476f
Pragati Rawat <pragatidubey2011@gmail.com>
2026-02-06 21:40:53 +0530

    initial commit: Project setup for BerlinClock using swift, swiftUi, swift testing & XC UI test

---
+++ BerlinClock/BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by Pragati RAWAT on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockTests.swift
+//  BerlinClockTests
+//
+//  Created by Pragati RAWAT on 06/02/2026.
+//
+
+import Testing
+@testable import BerlinClock
+
+struct BerlinClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
