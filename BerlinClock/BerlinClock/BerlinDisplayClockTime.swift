a301c2b3d435963274ae8bb2a647504cacbf82e5
Pragati Rawat <pragatidubey2011@gmail.com>
2026-02-08 22:26:21 +0530

    refactor : refactor the service layer with BerlinClockDisplayTime values and update test case as well

---
+++ BerlinClock/BerlinClock/BerlinDisplayClockTime.swift
@@ -0,0 +1,12 @@
+//
+//  BerlinDisplayClockTime.swift
+//  BerlinClock
+//
+//  Created by Pragati RAWAT on 08/02/2026.
+//
+
+struct BerlinDisplayClockTime: Equatable {
+    let hours: Int
+    let minutes: Int
+    let seconds: Int
+}
