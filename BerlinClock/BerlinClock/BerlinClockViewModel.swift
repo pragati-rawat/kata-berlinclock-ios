269ff557101bb435a8ffd213e541bd5ac0be05cf
Pragati Rawat <pragatidubey2011@gmail.com>
2026-02-09 17:32:44 +0530

    refactor: code refactoring for handle tick function

--- BerlinClock/BerlinClock/BerlinClockViewModel.swift
+++ BerlinClock/BerlinClock/BerlinClockViewModel.swift
@@ -8,12 +8,12 @@ import Foundation
 import Combine
 
 final class BerlinClockViewModel {
-    
+
     private let clockService: BerlinClockServiceProtocol
     private let composer: BerlinClockTimeComposerProtocol
-    
+
     @Published private(set) var composedTimeState: BerlinClockComposeTimeState?
-    
+
     private var isStarted = false
 
     init(clockService: BerlinClockServiceProtocol, composer: BerlinClockTimeComposerProtocol) {
@@ -21,24 +21,28 @@ final class BerlinClockViewModel {
         self.composer = composer
         self.composedTimeState = nil
     }
-    
+
     func startEmittingTime() {
         guard !isStarted else { return }
         isStarted = true
-        
+
         clockService.start { [weak self] time in
-            guard let self else { return }
-            self.composedTimeState = self.composer.composeClockTime(
-                hours: time.hours,
-                minutes: time.minutes,
-                seconds: time.seconds
-            )
+            self?.handleTick(time)
         }
     }
-    
+
     func stop() {
         guard isStarted else { return }
         isStarted = false
         clockService.stop()
+    }
+
+    // MARK: - Tick handling
+    private func handleTick(_ time: BerlinDisplayClockTime) {
+        composedTimeState = composer.composeClockTime(
+            hours: time.hours,
+            minutes: time.minutes,
+            seconds: time.seconds
+        )
     }
 }
